664b797a82e7cdf194bb55876297b8e2d46c4bd5
Ricardo Aviles <raviles69@fullsail.edu>
2013-01-31 01:29:38 -0500

    Corrected mistake on email code

--- library.project4.js
+++ library.project4.js
@@ -3,8 +3,11 @@
 // Assignment Project 4
 // 01/30/2013
 
-// My Library String
+// My Library
 
+// String Functions
+
+// Does a string follow a 123-456-7890 pattern like a phone number?
 var myLibraryString = function () {
     var checkPhoneNumber = function (string) {
         var numberCheck = /\d{3}\-\d{3}\-\d{4}/;
@@ -12,18 +15,23 @@ var myLibraryString = function () {
         return numberCheck.test (string);
     };
     
+// Does a string follow an aaa@bbb.ccc pattern like an email address?
     var checkEmail = function (string) {
-        var emailCheck = /([\w-\.]+)@((?:[\w] +\.)+)(a-zA-Z]{2,4})/;
+        var emailCheck = /^([0-9a-zA-Z]+([_.-]?[0-9a-zA-Z]+)*@[0-9a-zA-Z]+[0-9,a-z,A-Z,.,-]*(.){1}[a-zA-Z]{2,4})+$/;
         
         return emailCheck.test (string);
     };
     
+// Is the string a URL (Does it start with http:// or https://)?
     var checkUrl = function (string) {
         var urlCheck = /^(?:http|https):/;
         
         return urlCheck.test (string);
     };
     
+/* Given a string that is a list of things separated by a given string, as well as another
+string separator, return a string with the first separator changed to the second: Òa,b,cÓ +
+Ò,Ó + Ò/Ó --> Òa/b/cÓ.*/
     var changeInfo = function (string,newInfo) {
         var infoChange = /\W/g;
         
@@ -38,14 +46,16 @@ var myLibraryString = function () {
         };
 };
 
-// My Library Numbers
+// Numbers Functions
 
+/*Given a string version of a number, such as Ò42Ó, return the value as an actual Number
+data type, such as 42.*/
 var myLibraryNumber = function () {
     var numString = function (number) {
         return Number (number);
     };
     
-
+// Format a number to use a specific number of decimal places as for money: 2.1 --> 2.10.
     var decimalNumber = 234.8769
         numberDecimal = decimalNumber.toFixed(2);
     
@@ -55,16 +65,12 @@ var myLibraryNumber = function () {
     };
 };
 
-// Tests
-
-// String
-
 console.log ("Results of testing strings: ");
 
 var libStrings = myLibraryString ();
 
 console.log (libStrings.checkPhoneNumber ("888-867-5309"));
-console.log (libStrings.checkEmail ("johndoe@chacho.com"));
+console.log (libStrings.checkEmail ("Aricardo@yahoo.com"));
 console.log (libStrings.checkUrl ("http://msi.com"));
 console.log (libStrings.changeInfo ("a,e,i", "/"));
 
